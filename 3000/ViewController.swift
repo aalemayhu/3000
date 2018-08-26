103f3e3c0643a92eeb6cc3efb4477df33204a52b
Alexander Alemayhu <alexander@alemayhu.com>
2018-08-26 14:19:53 +0200

    Initial Commit

---
+++ 3000/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  3000
+//
+//  Created by Alexander Alemayhu on 26/08/2018.
+//  Copyright © 2018 Alexander Alemayhu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
