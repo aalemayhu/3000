d355d02e1bb524f62157569430d7c012c2a4da20
Alexander Alemayhu <alexander@alemayhu.com>
2018-09-04 18:42:56 +0200

    Start moving player buttons and info into a separate view

---
+++ 3000/Views/PlayerItemView.swift
@@ -0,0 +1,21 @@
+//
+//  PlayerItemView.swift
+//  3000
+//
+//  Created by ccscanf on 04/09/2018.
+//  Copyright © 2018 Alexander Alemayhu. All rights reserved.
+//
+
+import Cocoa
+
+class PlayerItemView: NSView {
+    
+    // TODO: add outlets for all members used in ViewController
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
