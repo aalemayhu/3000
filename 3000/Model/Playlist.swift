1ea56c71b3ce4a8f64536b3c1bc70c8a45cfe7e1
Alexander Alemayhu <alexander@alemayhu.com>
2018-09-13 19:01:57 +0200

    WIP: supporting nested folders
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- 3000/Model/Playlist.swift
+++ 3000/Model/Playlist.swift
@@ -41,7 +41,41 @@ class Playlist {
         return metadata
     }
     
+    // WIP: supporting nested folders
+//    func loadFiles(_ folder: URL) -> [TrackMetadata] {
+//        var metadata = [TrackMetadata]()
+//        // Traverse the directory for audio files
+//        let tracks = self.allTracks(from: folder)
+//        for track in tracks {
+//            let asset = AVURLAsset(url: track, options: PlayerManager.AssetOptions)
+//            metadata.append(TrackMetadata.load(asset: asset))
+//        }
+//        return metadata
+//    }
+    
     private func isSupported(_ type: String) -> Bool {
         return type.hasSuffix(".mp3")
+    }
+    
+    private func allTracks(from: URL) -> [URL] {
+        var all = [URL]()
+        
+        do {
+            let files = try FileManager.default.contentsOfDirectory(at: from, includingPropertiesForKeys: nil, options: [])
+            
+            // Get all of the top level tracks
+            all += files.filter { return self.isSupported($0.lastPathComponent.lowercased()) }
+            
+            // Get tracks in all subdirectories
+            files.forEach({ (u) in
+                var isDir: ObjCBool = false
+                FileManager.default.fileExists(atPath: u.absoluteString, isDirectory: &isDir)
+                if isDir.boolValue {
+                    all += allTracks(from: u)
+                }
+            })
+        } catch { debug_print("\(error)")  }
+        
+        return all
     }
 }
