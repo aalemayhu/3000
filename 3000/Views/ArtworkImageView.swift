0d8a042c07d3aeb69474ce6c9f41b8d63cf4e1d5
Alexander Alemayhu <alexander@alemayhu.com>
2018-09-28 18:56:39 +0200

    Use the intrinsic content size for the image
    This avoids the pixelation stuff.

--- 3000/Views/ArtworkImageView.swift
+++ 3000/Views/ArtworkImageView.swift
@@ -7,7 +7,8 @@ import Cocoa
 class ArtworkImageView: DropView {
     
     private var image: NSImage?
-    
+    private let minimumArtworkSize = NSSize(width: 250, height: 250)
+
     required init?(coder decoder: NSCoder) {
         super.init(coder: decoder)
     }
@@ -18,6 +19,23 @@ class ArtworkImageView: DropView {
     
     func configure(with image: NSImage?) {
         self.image = image
+        self.invalidateIntrinsicContentSize()
         self.needsDisplay = true
+    }
+    
+    override var intrinsicContentSize: NSSize {
+        get {
+            guard let image = self.image else { return self.minimumArtworkSize }
+            // Make sure the artwork size is not too small
+            if image.size.width < self.minimumArtworkSize.width ||
+                image.size.height < self.minimumArtworkSize.height {
+                return self.minimumArtworkSize
+            }
+            if image.size.width != image.size.height {
+                debug_print("Image not quadratic, fallingback on using width for the size")
+                return NSSize(width: image.size.width, height: image.size.width)
+            }
+            return image.size
+        }
     }
 }
